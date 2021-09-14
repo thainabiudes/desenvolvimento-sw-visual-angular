6787b4bccbe8fceb4d06148c9fbb0251a8774b51
Thaina Biudes <thaina.biudes@gmail.com>
2021-09-13 22:23:30 -0300

    feat: adiciona projeto inicial

---
+++ src/app/services/produto.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Produto } from "../models/produto";
+
+@Injectable({
+  providedIn: "root",
+})
+export class ProdutoService {
+  baseUrl = "http://localhost:5000/api/produto/";
+
+  constructor(private http: HttpClient) {}
+
+  list(): Observable<Produto[]> {
+    return this.http.get<Produto[]>(`${this.baseUrl}list`);
+  }
+
+  getById(id: number): Observable<Produto[]> {
+    return this.http.get<Produto[]>(`${this.baseUrl}getById/${id}`);
+  }
+}
